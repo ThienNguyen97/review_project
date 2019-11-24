36d2edbd5dc26f2f94672d857ce2abf6b38802a0
Thien <menkisede2@gmail.com>
2019-11-25 05:04:51 +0700

    admin

---
+++ app/models/ability.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, Place
+    can :read, Post
+
+    if user.present?
+      can :show, User, id: user.id
+
+      if user.has_role? :moderator
+        can [:new, :create, :admin_index], Place
+        can [:admin_show, :edit, :update, :destroy], Place,
+          id: Place.with_role(:moderator, user).pluck(:id)
+        can :manage, :all if user.has_role? :admin
+      end
+    end
+  end
+end
