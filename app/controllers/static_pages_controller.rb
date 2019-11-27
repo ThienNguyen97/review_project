7c2769ac39e26e5d0fd239da420c7b181942cd18
Thien <menkisede2@gmail.com>
2019-11-27 21:57:29 +0700

    edit request

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,8 @@
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @places = Place.popular_places
+    @posts = Post.popular_posts
+    @list_name = Place.all.pluck(:name)
+    @list_address = Place.all.pluck(:address)
+  end
 end
